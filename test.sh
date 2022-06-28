2f144dfb1041ce9aabbd548ae41b9a1f37764429
VeljkoDencic1 <vdencic@griddynamics.com>
2022-06-28 10:19:17 +0200

    prvi pokusaj

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# svaki=($(grep -o '[[:alnum:]]\+' ./test.txt))
+#  for x in "${svaki[@]}"; do
+#    USE=($(grep -E "=" ./test.txt))
+#     if [ $USE == $svaki ] ; then
+#       echo 
+#      fi
+#  done
+# # USE=$[grep "echo" ./test.txt]
+# # if [ $USE == $duplikat] ; then
+# # 	echo"$USE"
+# # fi
+
+
+# definisani=$(cut -d "=" -f1 ./test.txt)
+# echo $definisani
+
+# ispis=$(cut -d "$" -f2  ./test.txt)
+# echo $ispis
+
+# if($definisani == $ispis)
+
